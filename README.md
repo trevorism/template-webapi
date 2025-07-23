8d448ba4a0d807fcdd5499c8e86e4bc91ddba935
Trevor Brooks <trevorism@gmail.com>
2025-07-23 09:38:39 -0400

    Java 21 template

---
+++ README.md
@@ -0,0 +1,8 @@
+# gcloud
+![Build](https://github.com/trevorism/gcloud/actions/workflows/deploy.yml/badge.svg)
+![GitHub last commit](https://img.shields.io/github/last-commit/trevorism/gcloud)
+![GitHub language count](https://img.shields.io/github/languages/count/trevorism/gcloud)
+![GitHub top language](https://img.shields.io/github/languages/top/trevorism/gcloud)
+
+# How to build
+`gradle clean build`
---
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -0,0 +1,27 @@
+package com.trevorism
+
+import groovy.transform.CompileStatic
+import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "App",
+                version = "0.1.0",
+                description = "API",
+                contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
+        )
+)
+@CompileStatic
+class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application)
+
+    static void main(String[] args) {
+        log.info("Started application")
+        Micronaut.run(Application, args)
+    }
+}
